d965d38c54e1c9d0c384aea441bd3ecc4c8bf127
noxi515 <tzo.ppl@gmail.com>
2022-09-20 17:02:38 +0900

    Configure bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+// NgModuleのBootstrap
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
+
+// StandaloneのBootstrap
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+  ]
+})
+  .catch(err => console.error(err));
+
